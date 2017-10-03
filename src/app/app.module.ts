20f84a56c857fa0759b6bdda566f6aabaaa6ded2
EnricoSchw <enrico.schwe@gmx.de>
2017-10-03 15:04:58 +0200

    Add basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
+import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { TasksPage } from '../pages/tasks/tasks';
+import { GroupsPage } from '../pages/groups/groups';
+import { ChatPage } from '../pages/chat/chat';
+import { LoginPage } from '../pages/login/login';
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -12,18 +14,22 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    ListPage
+    TasksPage,
+    GroupsPage,
+    ChatPage,
+    LoginPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
+    IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
-    ListPage
+    TasksPage,
+    GroupsPage,
+    ChatPage,
+    LoginPage
   ],
   providers: [
     StatusBar,
