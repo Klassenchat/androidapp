1184ebd2a27c3377b126c711a7740bec55849103
EnricoSchw <enrico.schwe@gmx.de>
2017-10-03 15:06:05 +0200

    Add needed Pages

---
+++ src/pages/login/login.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import {TasksPage} from "../tasks/tasks";
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html'
+})
+export class LoginPage {
+
+  constructor(public navCtrl: NavController) {
+  }
+
+
+  login() {
+    this.navCtrl.setRoot(TasksPage);
+  }
+
+}
